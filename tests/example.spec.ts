7da18e0eea722ed075f966410ab4f1c91538e19b
Hisoka2000 <gamewalkthrougher@gmail.com>
2024-12-20 21:52:04 +0400

    updated has title test to use localhost

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:4200');
 
+  console.log(page.title());
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle('playwright-test-admin Demo Application');
 });
 
 test('get started link', async ({ page }) => {
